186bbeb4368e120f009564c4e0682761c75fe364
tv0823 <tv0823@bs.amalnet.k12.il>
2024-09-23 21:01:08 +0300

    Initial commit

---
+++ src/Animal.java
@@ -0,0 +1,11 @@
+public class Animal
+{
+    public void makeSound()
+    {
+        System.out.println("Sound");
+    }
+    public void move()
+    {
+        System.out.println("moving");
+    }
+}
